ae4b20a83672bc1dcc11c7dcda7065636adaf2e5
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-08-28 11:03:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let currentInput = document.getElementById('c1');
+
+function handleInput(input) {
+  if (input.value.length == 1) {
+    currentInput = input.nextElementSibling;
+    if (currentInput) {
+      currentInput.focus();
+      currentInput.select();
+    }
+  }
+}
+
+function handleBackspace(input) {
+  if (input.value == "" && input.previousElementSibling) {
+    currentInput = input.previousElementSibling;
+    currentInput.focus();
+    currentInput.select();
+  }
+}
