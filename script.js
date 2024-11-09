18b9435c9e9eac00be7826e2984613391233bccc
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-11-09 13:57:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-function handleInput(input) {
-  // Move to the next input when a number is entered
-  if (input.value.length === 1) {
-    const nextInput = input.nextElementSibling;
-    if (nextInput) {
-      nextInput.focus();
-    }
-  }
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const codes = document.querySelectorAll(".code");
 
-function handleBackspace(event, input) {
-  // Handle backspace key press
-  if (event.key === "Backspace" && input.value === "") {
-    const previousInput = input.previousElementSibling;
-    if (previousInput) {
-      previousInput.focus();
-    }
-  }
-}
+    codes.forEach((code, index) => {
+        code.addEventListener("input", (e) => {
+            // Move to the next input if a number is entered
+            if (e.target.value.length === 1) {
+                if (index < codes.length - 1) {
+                    codes[index + 1].focus();
+                }
+            }
+        });
+
+        code.addEventListener("keydown", (e) => {
+            // Move to the previous input on backspace if the current input is empty
+            if (e.key === "Backspace" && e.target.value === "") {
+                if (index > 0) {
+                    codes[index - 1].focus();
+                }
+            }
+        });
+    });
+});
